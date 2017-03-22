81839f9a7eb440bf3ace472b8b79afeeacdc165d
Fadlika Dita Nurjanto <fadlika@kidsoft.com.au>
2017-03-22 09:55:38 +0700

    add modals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ModalsPage } from '../pages/modals/modals';
+
+import { AngularFireModule } from 'angularfire2';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyA3_niQfuGzCG_PdPX3v-rYkIQuFRlBwTY",
+    authDomain: "movie-ionic-2-firebase.firebaseapp.com",
+    databaseURL: "https://movie-ionic-2-firebase.firebaseio.com",
+    storageBucket: "movie-ionic-2-firebase.appspot.com",
+    messagingSenderId: "921599744923"
+};
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ModalsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ModalsPage
   ],
   providers: [
     StatusBar,
